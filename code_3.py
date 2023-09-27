4ea2c32e5e35c20d2e23503ef191919046637fe7
Lora <143578889+Lora207@users.noreply.github.com>
2023-09-27 06:28:37 +0200

    Create code_3.py

---
+++ code_3.py
@@ -0,0 +1,69 @@
+    for (let i = 0; i < 10; i++) {
+function generateRandomData() {
+    const data = [];
+}
+function main() {
+}
+        data.push(Math.floor(Math.random() * 100) + 1);
+main();
+    const data = generateRandomData();
+        console.log("Random Number: " + item);
+    }
+
+
+    return data;
+    data.forEach(item => {
+    });
+public class RandomDataGenerator {
+
+        }
+
+        for (int i = 0; i < 10; i++) {
+}
+        for (int item : data) {
+        }
+    public static void main(String[] args) {
+        int[] data = new int[10];
+            System.out.println("Random Number: " + item);
+            data[i] = (int) (Math.random() * 100) + 1;
+    }def generate_random_data():
+
+
+
+
+def main():
+if __name__ == "__main__":
+    return data
+    data = generate_random_data()
+        print(f"Random Number: {item}")
+import random
+    data = [random.randint(1, 100) for _ in range(10)]
+    for item in data:
+    main()function main() {
+function generateRandomData() {
+        data.push(Math.floor(Math.random() * 100) + 1);
+    const data = generateRandomData();
+    for (let i = 0; i < 10; i++) {
+main();
+}
+    data.forEach(item => {
+}
+        console.log("Random Number: " + item);
+
+    });
+    const data = [];
+    }
+    return data;
+
+
+        for (int item : data) {
+        }
+}
+        }
+public class RandomDataGenerator {
+            data[i] = (int) (Math.random() * 100) + 1;
+        int[] data = new int[10];
+            System.out.println("Random Number: " + item);
+    public static void main(String[] args) {
+    }
+        for (int i = 0; i < 10; i++) {
